efcb87f2afa21744af01af126781397e4158cfe7
KgaugeloEliasMatlou <eliasmatlou66@gmail.com>
2022-06-22 12:55:23 +0200

    Add project files.

---
+++ ParamskeywordInMethods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static double Findproduct(params double[] values)
+    {
+        double product = 1;
+
+        foreach(var d in values)
+        {
+            product *= d;
+        }
+        return product;
+    }
+    static void Main()
+    {
+        Console.WriteLine("1*2={0} ", Findproduct(1, 2));
+        Console.WriteLine("2*2*3={0} ",Findproduct(2,2, 3));
+
+    }
+}
